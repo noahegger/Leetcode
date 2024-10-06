82412075484209f8fc0a2a0352a2b5ac4c199cb4
Noah <70112580+noahegger@users.noreply.github.com>
2024-10-06 17:07:14 -0500

    Time: 724 ms (68.65%), Space: 27.5 MB (47.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,17 +2,14 @@
 # Can we go through the prices only once?
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
+        profit = 0
         min_price = prices[0]
 
-        for price in prices:
+        for price in prices[1:]:
             if price < min_price:
                 min_price = price
-            else:
-                profit = price - min_price
-                if profit > max_profit:
-                    max_profit = profit
-        return max_profit
+            profit = max(profit, price-min_price)
+        return profit
 
         
         
