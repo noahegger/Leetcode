189c468da4fdfc84af9ef1ba5aceece72915e86b
Noah <70112580+noahegger@users.noreply.github.com>
2024-10-06 17:02:09 -0500

    Time: 679 ms (93.39%), Space: 27.6 MB (13.06%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,18 @@
+# Brute force nested for loop is bad
+# Can we go through the prices only once?
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = prices[0]
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                profit = price - min_price
+                if profit > max_profit:
+                    max_profit = profit
+        return max_profit
+
+        
+        
