38d071439d6dc038bd4ed3125a8644c5e55bf6df
Noah <70112580+noahegger@users.noreply.github.com>
2024-10-07 16:17:52 -0500

    Time: 558 ms (25.11%), Space: 31.1 MB (47.38%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,16 @@
+# Compute max sum up to and including given endpoint
+# If sum with endpoint[i] included is less than num[i] at that point,
+# use num[i] as new sum
+# Only update res (maximum sum) if the new max sum is greater
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        res = nums[0]
+        max_sum = nums[0]
+
+        for i in range(1,len(nums)):
+            max_sum = max(max_sum+nums[i], nums[i])
+            res = max(res, max_sum)
+        return res
+
+        
