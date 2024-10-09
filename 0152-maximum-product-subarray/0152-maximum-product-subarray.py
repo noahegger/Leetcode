9fb5039474787bbe045b38b877dcad75fd74b749
Noah <70112580+noahegger@users.noreply.github.com>
2024-10-09 14:56:32 -0500

    Time: 139 ms (5.22%), Space: 17.1 MB (40.91%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,29 @@
+# If no zeros, and even # of negatives, max product will be the entire array
+# If no zeros, off # of negatives, need to split sub arrays on negatives and compare
+# If zeros, need to split sub arrays on zeros, and then check negative cases...
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        max_thus = min_thus = result = nums[0]
+        for num in nums[1:]:
+            # Store the current max_thus in a temporary variable
+            print("max_thus:", max_thus)
+            print("min_thus:", min_thus)
+            print("result:", result)
+            temp_max = max_thus
+            # Calculate the new max_thus and min_thus
+            max_thus = max(num, num * max_thus, num * min_thus)
+            min_thus = min(num, num * temp_max, num * min_thus)
+
+            print("new_max", max_thus)
+            print("new_min", min_thus)
+            
+            # Update the result with the maximum value of result and max_thus
+            result = max(result, max_thus)
+
+        return result
+            
+
+            
+        
