3036acb804782337737d3ee32a33486c8a76ab72
Noah <70112580+noahegger@users.noreply.github.com>
2024-10-09 14:59:08 -0500

    Time: 77 ms (25.81%), Space: 17.1 MB (40.91%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -7,19 +7,9 @@ class Solution:
         
         max_thus = min_thus = result = nums[0]
         for num in nums[1:]:
-            # Store the current max_thus in a temporary variable
-            print("max_thus:", max_thus)
-            print("min_thus:", min_thus)
-            print("result:", result)
             temp_max = max_thus
-            # Calculate the new max_thus and min_thus
             max_thus = max(num, num * max_thus, num * min_thus)
             min_thus = min(num, num * temp_max, num * min_thus)
-
-            print("new_max", max_thus)
-            print("new_min", min_thus)
-            
-            # Update the result with the maximum value of result and max_thus
             result = max(result, max_thus)
 
         return result
