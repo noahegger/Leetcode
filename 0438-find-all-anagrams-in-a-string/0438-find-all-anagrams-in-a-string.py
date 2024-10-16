a43afe26284ee95f3eab3f04209070fe5abe8fff
Noah <70112580+noahegger@users.noreply.github.com>
2024-10-16 14:57:07 -0500

    Time: 326 ms (15.9%), Space: 17.8 MB (63.06%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,13 +1,51 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        sort_p = sorted(p)
+
+        # First solution relying on sorted function
+        # sort_p = sorted(p)
+        # indices = []
+        # r = len(p)-1
+        # l = 0
+
+        # while r < len(s):
+        #     if sorted(s[l:r+1]) == sort_p:
+        #         indices.append(l)
+        #     r += 1
+        #     l += 1
+        # return indices
+
+        # Second solution using hashmap to count frequencies
+
+        count_map, p_map = {}, {}
         indices = []
-        r = len(p)-1
-        l = 0
+        r = l = 0
+
+        for char in p:
+            if char not in p_map:
+                p_map[char] = 1
+            else:
+                p_map[char] += 1
 
         while r < len(s):
-            if sorted(s[l:r+1]) == sort_p:
+            print(r)
+            print(count_map)
+            if s[r] not in count_map:
+                count_map[s[r]] = 1
+            else:
+                count_map[s[r]] += 1
+
+            if r - l + 1 > len(p):
+                # print(r, l)
+                count_map[s[l]] -= 1
+                
+                if count_map[s[l]] == 0:
+                    count_map.pop(s[l])
+                l += 1
+            
+
+            if count_map == p_map:
                 indices.append(l)
+
             r += 1
-            l += 1
+        
         return indices
