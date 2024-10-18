3e7d5a9a0d89cb83d390c100991f8cc129f8af5c
Noah <70112580+noahegger@users.noreply.github.com>
2024-10-18 14:58:39 -0500

    Time: 852 ms (5%), Space: 17.2 MB (85.34%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,50 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def check_condition(seen_map, t_map):
+            return all(char in seen_map and seen_map[char] >= count for char, count in t_map.items())
+
+        # Start expanding right until all of t is in s
+        # Once window is found, shrink from the left until condition no longer satisfied
+        # continue expanding right
+        # Return "" if condition never met
+
+        # s = s.lower()
+        # t = t.lower()
+        if len(t) > len(s):
+            return ""
+
+        t_map = {}
+        seen_map = {}
+        for char in t:
+            if char not in t_map:
+                t_map[char] = 1
+            else:
+                t_map[char] += 1
+        
+        l = r = 0
+        min_substring = ""
+        min_indices = None
+        while r < len(s):
+            print(min_indices)
+            if s[r] not in seen_map:
+                seen_map[s[r]] = 1
+            else:
+                seen_map[s[r]] += 1
+            
+            condition = check_condition(seen_map, t_map)
+            while condition:
+                # print(r, l)
+                # print(min_indices[1] - min_indices[0])
+                # print(r-l)
+                # print(min_indices)
+                if min_indices is None or r-l+1 <= min_indices[1] - min_indices[0]:
+                    min_indices = (l, r+1)
+                    # print(s[min_indices[0]:min_indices[1]]) 
+                if s[l] in seen_map:
+                    seen_map[s[l]] -= 1
+                l += 1
+                condition = check_condition(seen_map, t_map)
+            
+            r+=1
+
+        return "" if min_indices is None else s[min_indices[0]:min_indices[1]]
