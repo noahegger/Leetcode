55c6acfd81a820b17b30c2d8c7dfd073455c3a36
Noah <70112580+noahegger@users.noreply.github.com>
2024-10-19 13:31:56 -0500

    Time: 230 ms (94.34%), Space: 16.5 MB (95.23%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,11 +1,10 @@
 class Solution:
     def expand_l_r(self, l_index: int, r_index: int, s:str) -> str:
-
         while l_index >= 0 and r_index < len(s) and s[l_index] == s[r_index]:
             l_index-=1
             r_index+=1
-        
         return s[l_index+1:r_index]
+
     def longestPalindrome(self, s: str) -> str:
          
         res = ""
@@ -15,7 +14,7 @@ class Solution:
             if len(palindrome_o) > len(res):
                 res = palindrome_o
             # check even palindrome
-            palindrome_e = self. expand_l_r(i, i+1, s)
+            palindrome_e = self.expand_l_r(i, i+1, s)
             if len(palindrome_e) > len(res):
                 res = palindrome_e
         
