92b27cf37cecc43293929c5b2d8dd2dc5d4458b8
Noah <eggernoah1@gmail.com>
2024-10-24 11:33:29 -0500

    Update pull_to_obsidian.py

--- pull_to_obsidian.py
+++ pull_to_obsidian.py
@@ -4,7 +4,9 @@ import shutil
 # Path to your GitHub repo
 repo_path = "/Users/noahegger/git/Leetcode"
 # Path to your Obsidian vault
-obsidian_vault_path = "/Users/noahegger/Documents/Obsidian Vault/LeetCode Problems"
+obsidian_vault_path = (
+    "/Users/noahegger/Documents/Obsidian Vault/Programming/LeetCode Problems"
+)
 
 
 # Function to format the content
@@ -22,17 +24,20 @@ def format_leetcode_file(problem_name):
     with open(solution_path, "r") as f:
         solution_code = f.read()
 
-    # Create Markdown content
-    markdown_content = f"# {problem_name} Leetcode Problem\n\n"
-    markdown_content += f"## Problem Description\n{problem_description}\n\n"
+    # Create Markdown content without the title
+    markdown_content = f"## Problem Description\n{problem_description}\n\n"
     markdown_content += "## My Solution\n```python\n" + solution_code + "\n```\n"
 
     return markdown_content
 
 
-# Function to save the Markdown file
 def save_to_obsidian(problem_name, content):
     obsidian_file_path = os.path.join(obsidian_vault_path, f"{problem_name}.md")
+
+    # Create the directory if it does not exist
+    os.makedirs(obsidian_vault_path, exist_ok=True)
+
+    # Write content to the file, overwriting if it already exists
     with open(obsidian_file_path, "w") as f:
         f.write(content)
     print(f"Saved {problem_name} to {obsidian_file_path}.")
