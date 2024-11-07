530c8e4319e1877381868d85609c31a2ba0ad2ac
Noah <70112580+noahegger@users.noreply.github.com>
2024-11-07 12:28:14 -0600

    Time: 131 ms (65.23%), Space: 58.8 MB (81.43%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, combo):
+            if len(combo) == k:
+                res.append(combo[:]) # need to give copy?
+                return
+
+            for i in range(start, n+1):
+                combo.append(i)
+                backtrack(i+1, combo)
+                combo.pop()
+        
+        backtrack(1, [])
+        return res
+
+
+    
+    
