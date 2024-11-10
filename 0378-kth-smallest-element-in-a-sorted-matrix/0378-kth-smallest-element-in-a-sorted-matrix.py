7d44121de4937928a434d6eebf92ff5c67c03d61
Noah <70112580+noahegger@users.noreply.github.com>
2024-11-10 10:23:34 -0600

    Time: 1 ms (86.81%), Space: 20.3 MB (88.73%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -5,39 +5,39 @@ class Solution:
         n = len(matrix)
 
         # Binary search approach
-        # def count_less_than_equal(x):
-        #     count = 0
-        #     j = n-1 #last column
+        def count_less_than_equal(x):
+            count = 0
+            j = n-1 #last column
 
-        #     for i in range(n): #loop through each row
-        #         while j>=0 and matrix[i][j] > x: # while element is > x, decrement column
-        #             j -= 1
-        #         count += (j+1) #remaining # of columns to the left is how many are < x
-        #     # after continuing through each row, count tells how many elements < x
-        #     return count
+            for i in range(n): #loop through each row
+                while j>=0 and matrix[i][j] > x: # while element is > x, decrement column
+                    j -= 1
+                count += (j+1) #remaining # of columns to the left is how many are < x
+            # after continuing through each row, count tells how many elements < x
+            return count
 
-        # L = matrix[0][0]
-        # R = matrix[-1][-1]
+        L = matrix[0][0]
+        R = matrix[-1][-1]
 
-        # while L < R:
-        #     M = (L+R)//2
-        #     if count_less_than_equal(M) < k:
-        #         L = M + 1
-        #     else:
-        #         R = M
+        while L < R:
+            M = (L+R)//2
+            if count_less_than_equal(M) < k:
+                L = M + 1
+            else:
+                R = M
 
-        # return L
+        return L
 
         # Min-Heap Approach
-        min_heap = []
-        for r in range(min(k, n)): #initialize heap with min number of rows needed
-            heapq.heappush(min_heap, (matrix[r][0], r, 0)) #add first column of each row
+        # min_heap = []
+        # for r in range(min(k, n)): #initialize heap with min number of rows needed
+        #     heapq.heappush(min_heap, (matrix[r][0], r, 0)) #add first column of each row
         
-        for _ in range(k):
-            element, r, c = heapq.heappop(min_heap) 
-            # remove the minimum element and push the subsequent column
-            # if the column pushed is greater than an existing, we will automatically go to the next row
-            if c+1 < n:
-                heapq.heappush(min_heap, (matrix[r][c+1], r, c+1))
-            # after k times, we will have kth smallest
-        return element
+        # for _ in range(k):
+        #     element, r, c = heapq.heappop(min_heap) 
+        #     # remove the minimum element and push the subsequent column
+        #     # if the column pushed is greater than an existing, we will automatically go to the next row
+        #     if c+1 < n:
+        #         heapq.heappush(min_heap, (matrix[r][c+1], r, c+1))
+        #     # after k times, we will have kth smallest
+        # return element
