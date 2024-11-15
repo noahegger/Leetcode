5dead092405e9b6d86873324d2628998d4e96ea4
Noah <70112580+noahegger@users.noreply.github.com>
2024-11-15 08:31:55 -0600

    Time: 0 ms (100%), Space: 22.3 MB (85.5%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -9,6 +9,7 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
 
+        # Tortoise & Hare Two-pointer to find mid
         def find_mid(head: ListNode) -> ListNode:
             slow = head
             fast = head
@@ -18,6 +19,7 @@ class Solution:
                 fast = fast.next.next
             return slow
         
+        # Reverse second half
         def reverse(head:ListNode)->ListNode:
             prev = None
             curr = head
@@ -28,6 +30,7 @@ class Solution:
                 curr = temp
             return prev
 
+        # Merge first half, reversed second half
         def merge_lists(l1:ListNode, l2:ListNode)-> None:
             while l2:
                 l1_next = l1.next
@@ -40,14 +43,20 @@ class Solution:
                 l1 = l1_next
                 l2 = l2_next
 
+        # Return if head is None or list is 1 val
         if not head or not head.next:
             return
         
+        # Find midpoint
         mid = find_mid(head)
+        # Define second half starting node
         half2 = mid.next
+        # Disconnect First from second
         mid.next = None
 
+        # Reverse second
         half2 = reverse(half2)
+        # Merge back
         merge_lists(head, half2)
        
 
