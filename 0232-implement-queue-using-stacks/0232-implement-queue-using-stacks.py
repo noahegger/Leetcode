1206a8ca5451aea115042778cf4c761b0488568e
Noah <70112580+noahegger@users.noreply.github.com>
2024-12-01 12:37:50 -0600

    Time: 0 ms (100%), Space: 17.7 MB (5.73%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -9,18 +9,27 @@ class MyQueue:
         self.stack1.append(x)
         
     def pop(self) -> int:
-        while self.stack1:
-            self.stack2.append(self.stack1.pop())
-        element = self.stack2.pop()
-        while self.stack2:
-            self.stack1.append(self.stack2.pop())
+        # while self.stack1:
+        #     self.stack2.append(self.stack1.pop())
+        # element = self.stack2.pop()
+        # while self.stack2:
+        #     self.stack1.append(self.stack2.pop())
 
-        return element
+        # return element
+
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
     def peek(self) -> int:
-        return self.stack1[0]
+        # return self.stack1[0]
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
 
     def empty(self) -> bool:
-        return not self.stack1
+        return not (self.stack1 or self.stack2)
         
 
 
