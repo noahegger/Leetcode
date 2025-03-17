88b85166dc489e8f58b7280844c5cbe5fc477adb
Noah <70112580+noahegger@users.noreply.github.com>
2025-03-17 11:13:41 -0500

    Time: 7 ms (48.95%), Space: 19.3 MB (52.17%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        def dfs(course, visited):
+
+            if not course_map[course]: # No dependency
+                return True
+            
+            if course in visited:
+                return False
+            
+            visited.add(course)
+
+            for p in course_map[course]:
+                if not dfs(p, visited):
+                    return False
+                    
+            course_map[course] = []
+            return True
+
+            # visited.add(course)
+            # for prereq in course_map[course]:
+            #     if prereq not in visited:
+            #         if not course_map[prereq]:
+            #             return True
+            #         dfs(prereq, visited)
+            #     else:
+            #         return False
+        
+        visited = set()
+        course_map = defaultdict(list)
+        for course, p in prerequisites:
+            course_map[course].append(p)
+
+        for course in range(numCourses):
+            if not dfs(course, visited):
+                return False
+        
+        return True
