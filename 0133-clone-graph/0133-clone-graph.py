9bb6cfdd4c2b6b320da685a4e7c37983c3ffc255
Noah <70112580+noahegger@users.noreply.github.com>
2025-04-14 16:45:10 -0500

    Time: 35 ms (94.3%), Space: 18.2 MB (39.87%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,19 +11,54 @@ from collections import deque
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         
-        old_new_map = {}
+        otn_map = {}
 
         def clone(node):
-            if node in old_new_map:
-                return old_new_map[node]
+            if node in otn_map:
+                return otn_map[node]
+            
+            node_copy = Node(node.val)
 
-            copy = Node(node.val)
-            old_new_map[node] = copy
-            
-            for nei in node.neighbors:
-                copy.neighbors.append(clone(nei))
-            
-            return copy
+            otn_map[node] = node_copy
+
+            for n in node.neighbors:
+                node_copy.neighbors.append(clone(n))
+
+            return node_copy
         
         return clone(node) if node else None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # old_new_map = {}
+
+        # def clone(node):
+        #     if node in old_new_map:
+        #         return old_new_map[node]
+
+        #     copy = Node(node.val)
+        #     old_new_map[node] = copy
+            
+        #     for nei in node.neighbors:
+        #         copy.neighbors.append(clone(nei))
+            
+        #     return copy
+        
+        # return clone(node) if node else None
 
